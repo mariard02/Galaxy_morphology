dfdee11788a9ed89df8a20ae278c3d9b53882779
María Rodríguez Domínguez <maria@Ordenador-portatil-de-Maria.local>
2024-10-10 19:11:44 +0200

    Created a class that finds the best fit for the Selsic profile of galaxies.

---
+++ analysis.py
@@ -0,0 +1,31 @@
+from galaxymorph import galaxy
+import matplotlib.pyplot as plt
+
+print("****************************************************************************************************")
+print("M87")
+
+file = "/Users/maria/Desktop/Máster/M1/S1/Galaxias/Morphology/Data/m87.dat"
+
+m87 = galaxy("M87", 16.4, file)
+m87.galaxy_info()
+m87.galaxy_fit1()
+
+print("****************************************************************************************************")
+print("M101")
+
+file = "/Users/maria/Desktop/Máster/M1/S1/Galaxias/Morphology/Data/m101.dat"
+
+m101 = galaxy("M101", 6.4, file)
+m101.galaxy_info()
+m101.galaxy_fit1()
+
+print("****************************************************************************************************")
+print("M81")
+
+file = "/Users/maria/Desktop/Máster/M1/S1/Galaxias/Morphology/Data/m81.dat"
+
+m81 = galaxy("M81", 3.6, file)
+m81.galaxy_info()
+m81.galaxy_fit2()
+
+#plt.show()
---
+++ galaxymorph.py
@@ -0,0 +1,121 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from astropy.modeling import models, fitting
+from astropy.modeling.models import Sersic1D
+
+class galaxy(object):
+	def __init__(self, name, distance, path):
+		self.name = name
+		self.distance = distance
+		self.path = path
+
+		self.arcsec, self.SB, self.SB_err = np.loadtxt(self.path, usecols=[0,1,2],unpack=True)
+
+		self.radius = self.distance * self.arcsec / 206
+
+	def galaxy_info(self):
+		print("Catalogue name: {}".format(self.name))
+		print("Distance = {} Mpc".format(self.distance))
+
+	def galaxy_plot(self):
+		fig = plt.figure(figsize = (8, 6))
+		plt.errorbar(self.radius, self.SB, yerr=self.SB_err, lw=2, markersize=4, fmt='o', color='deepskyblue', capsize=3)
+		plt.xlabel(r'radius [kpc]', fontsize=12) 
+		plt.ylabel(r'SB', fontsize=12) 
+		plt.xticks(size=12) 
+		plt.yticks(size=12)
+		plt.semilogy()
+		plt.title("Radial profile for {}".format(self.name))
+
+	def galaxy_fit1(self):
+		# Initiate function with initial guess for the parameters
+
+		Sersic_init = Sersic1D(amplitude = 0.5, r_eff = 1, n = 4.0)
+
+		# Fitting function
+		fit = fitting.LevMarLSQFitter()
+
+		# Perform the fit (note that uncertainties are also considered)
+		best_Sersic = fit(Sersic_init, self.radius, self.SB, weights = 1/self.SB_err, maxiter = 5000)
+
+		print("")
+		print(best_Sersic)
+
+		cov_diag = np.sqrt(np.diag(fit.fit_info['param_cov']))
+		print('error =', cov_diag)
+
+		fig, ax = plt.subplots(2, gridspec_kw={'height_ratios': [3, 1]}, figsize = (7, 7))
+
+		fig.suptitle("Best fit model for the radial profile of {}".format(self.name))
+
+		ax[0].plot(self.radius, best_Sersic(self.radius), label='Best-fit Sersic Model', color='grey', lw=2)
+		ax[0].errorbar(self.radius, self.SB, yerr=self.SB_err, fmt='o', label='Data', color='deepskyblue', markersize=4, capsize=3)
+		ax[0].set_xlabel('Radius (Mpc)')
+		ax[0].set_ylabel('Surface Brightness [counts/arcsec^2]')
+		ax[0].legend()
+		ax[0].semilogy()
+
+		ax[1].scatter(self.radius, (self.SB - best_Sersic(self.radius))/self.SB, color = 'deepskyblue')
+		ax[1].set_xlabel('Radius (Mpc)')
+		ax[1].set_ylabel('Residuals')
+		#ax[1].semilogy()
+
+	def galaxy_fit2(self):
+	    # Filtrar los datos para eliminar NaN o inf en radius, SB o SB_err
+	    mask = np.isfinite(self.radius) & np.isfinite(self.SB) & np.isfinite(self.SB_err)
+	    
+	    # Aplicar el filtro a los datos
+	    radius_filtered = self.radius[mask]
+	    SB_filtered = self.SB[mask]
+	    SB_err_filtered = self.SB_err[mask]
+
+	    # Iniciar las funciones de Sérsic con suposiciones iniciales para los parámetros
+	    Sersic1_init = Sersic1D(amplitude=0.1722277258438259, r_eff=2.6, n=1)  # Primer componente Sérsic
+	    Sersic2_init = Sersic1D(amplitude=0.11100932153044392, r_eff=3.4, n=4.20255893376516)  # Segundo componente Sérsic
+
+	    # Suma de dos funciones de Sérsic
+	    Sersic_total_init = Sersic1_init + Sersic2_init
+
+	    # Función de ajuste
+	    fit = fitting.LevMarLSQFitter()
+
+	    # Realizar el ajuste con los datos filtrados
+	    try:
+	        best_Sersic_total = fit(Sersic_total_init, radius_filtered, SB_filtered, weights=1/SB_err_filtered, maxiter=5000)
+	    except Exception as e:
+	        print(f"Error durante el ajuste: {e}")
+	        return
+
+	    print("")
+	    print(best_Sersic_total)
+
+	    # Cálculo de las incertidumbres (diagonal de la matriz de covarianza)
+	    cov_diag = np.sqrt(np.diag(fit.fit_info['param_cov']))
+	    print('error =', cov_diag)
+
+	    # Gráficos de los resultados
+	    fig, ax = plt.subplots(2, gridspec_kw={'height_ratios': [3, 1]}, figsize = (7, 7))
+
+	    fig.suptitle("Best fit model for the radial profile of {}".format(self.name))
+
+	    # Graficar el modelo ajustado (suma de los dos perfiles de Sérsic)
+	    ax[0].plot(radius_filtered, best_Sersic_total(radius_filtered), label='Best-fit Sersic Model (two components)', color='grey', lw=2)
+	    
+	    # Graficar los datos observacionales con barras de error
+	    ax[0].errorbar(radius_filtered, SB_filtered, yerr=SB_err_filtered, fmt='o', label='Data', color='deepskyblue', markersize=4, capsize=3)
+
+	    ax[0].set_xlabel('Radius (Mpc)')
+	    ax[0].set_ylabel('Surface Brightness [counts/arcsec^2]')
+	    ax[0].legend()
+	    ax[0].semilogy()
+
+	    # Graficar los residuales
+	    residuals = (SB_filtered - best_Sersic_total(radius_filtered)) / SB_filtered
+	    ax[1].scatter(radius_filtered, residuals, color='deepskyblue')
+	    ax[1].set_xlabel('Radius (Mpc)')
+	    ax[1].set_ylabel('Residuals')
+
+	    plt.tight_layout()
+	    #plt.show()
+
